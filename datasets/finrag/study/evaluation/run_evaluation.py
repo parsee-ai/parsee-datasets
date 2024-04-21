f964a0af0d74e23fb5402982dc854f2b751df772
Parsee.ai <t.flassbeck@simfin.com>
2024-04-21 16:28:38 +0200

    fixes

--- datasets/finrag/study/evaluation/run_evaluation.py
+++ datasets/finrag/study/evaluation/run_evaluation.py
@@ -14,24 +14,28 @@ OPENAI_KEY -> openai api key
 REPLICATE_API_TOKEN -> api key for replicate.com
 TOGETHER_KEY -> api key for together.xyz
 """
-def run_eval_by_file(csv_file_path: str, writer_path: str, multimodal: bool, token_limit: int = 4000):
+def run_eval_by_file(csv_file_path: str, writer_path: Optional[str], multimodal: bool, token_limit: int = 4000, use_cached_only: bool = False):
 
     if not multimodal:
         models = [
-            anthropic_config(os.getenv("ANTHROPIC_API_KEY"), "claude-3-opus-20240229", token_limit),
-            gpt_config(os.getenv("OPENAI_KEY"), token_limit, "gpt-4-1106-preview"),
-            replicate_config(os.getenv("REPLICATE_API_TOKEN"), "meta/meta-llama-3-70b-instruct", token_limit),
-            replicate_config(os.getenv("REPLICATE_API_TOKEN"), "meta/meta-llama-3-8b-instruct", token_limit),
-            replicate_config(os.getenv("REPLICATE_API_TOKEN"), "mistralai/mixtral-8x7b-instruct-v0.1", token_limit),
-            together_config(os.getenv("TOGETHER_KEY"), "mistralai/Mixtral-8x22B-Instruct-v0.1", token_limit),
-            together_config(os.getenv("TOGETHER_KEY"), "databricks/dbrx-instruct", token_limit)
+            anthropic_config(os.getenv("ANTHROPIC_API_KEY") if not use_cached_only else "n/a", "claude-3-opus-20240229", token_limit),
+            gpt_config(os.getenv("OPENAI_KEY") if not use_cached_only else "n/a", token_limit, "gpt-4-1106-preview"),
+            replicate_config(os.getenv("REPLICATE_API_TOKEN") if not use_cached_only else "n/a", "meta/meta-llama-3-70b-instruct", token_limit),
+            replicate_config(os.getenv("REPLICATE_API_TOKEN") if not use_cached_only else "n/a", "meta/meta-llama-3-8b-instruct", token_limit),
+            #replicate_config(os.getenv("REPLICATE_API_TOKEN") if not use_cached_only else None, "mistralai/mixtral-8x7b-instruct-v0.1", token_limit),
+            together_config(os.getenv("TOGETHER_KEY") if not use_cached_only else "n/a", "mistralai/Mixtral-8x22B-Instruct-v0.1", token_limit),
+            together_config(os.getenv("TOGETHER_KEY") if not use_cached_only else "n/a", "databricks/dbrx-instruct", token_limit)
         ]
     else:
         models = []
 
-    cloud = ParseeCloud(os.getenv('PARSEE_API_KEY'))
+    cloud = ParseeCloud(os.getenv('PARSEE_API_KEY') if not use_cached_only else None)
     template = cloud.get_template("661bd24fb129ff2526f2af09")
     reader = SimpleCsvDiskReader(csv_file_path)
-    writer = CsvDiskWriter(writer_path, False)
-    performance = evaluate_llm_performance(template, reader, models, None, writer, True, os.path.basename(csv_file_path).split(".")[0]+"_with_answers")
+    writer = None
+    writer_file_name = None
+    if writer_path is not None:
+        writer = CsvDiskWriter(writer_path, False)
+        writer_file_name = os.path.basename(csv_file_path).split(".")[0] + "_with_answers"
+    performance = evaluate_llm_performance(template, reader, models, None, writer, True, writer_file_name)
     return performance
